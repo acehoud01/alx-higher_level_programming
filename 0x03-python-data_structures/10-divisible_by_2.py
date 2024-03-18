a1d4df181e4b4b0e6d33ab66aa287ae0afbf119b
Anele Bilikwa <aceamahle1@gmail.com>
2024-03-18 21:07:20 +0200

    10-divisible_by_2.py

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def divisible_by_2(my_list=[]):
+    numbers = []
+    for i in my_list:
+        if i % 2 == 0:
+            numbers.append(True)
+        else:
+            numbers.append(False)
+    return numbers
