cbf050e873d59ba5a570d53c89324e32908ca57b
Anele Bilikwa <aceamahle1@gmail.com>
2024-04-02 20:39:51 +0200

    102-square.py

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,37 +1,53 @@
 #!/usr/bin/python3
+"""Module containing the Square class"""
+
+
 class Square:
+    """The Square class"""
     def __init__(self, size=0):
+        """Initializing an instance of Square
+        Args:
+            size (int): The size of the Square instance. Default value is 0.
+        """
         self.size = size
 
     @property
     def size(self):
+        """int: size of the Square instance"""
         return self.__size
 
     @size.setter
-    def size(self, value):
-        if not isinstance(value, int):
+    def size(self, size):
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        elif value < 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = value
+
+        self.__size = size
 
     def area(self):
+        """Returns the current square area of the instance
+        Returns:
+            int: The square of size
+        """
         return self.__size ** 2
 
-    def __eq__(self, other):
-        return self.area() == other.area()
+    def compare_area(self, other):
+        """Compare the area of this square with another square.
+        Args:
+            other (Square): Another square to compare the area with.
 
-    def __ne__(self, other):
-        return self.area() != other.area()
+        Returns:
+            int: 1 if the area of this square is greater than the area of the other square,
+                 0 if they are equal,
+                -1 if the area of this square is less than the area of the other square.
+        """
+        if not isinstance(other, Square):
+            raise TypeError("other must be an instance of Square")
 
-    def __gt__(self, other):
-        return self.area() > other.area()
-
-    def __ge__(self, other):
-        return self.area() >= other.area()
-
-    def __lt__(self, other):
-        return self.area() < other.area()
-
-    def __le__(self, other):
-        return self.area() <= other.area()
+        if self.area() > other.area():
+            return 1
+        elif self.area() < other.area():
+            return -1
+        else:
+            return 0
