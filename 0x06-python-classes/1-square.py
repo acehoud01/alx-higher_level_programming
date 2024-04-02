3a5111918cbf781ba7e635d502b6fe641bab92ec
Anele Bilikwa <aceamahle1@gmail.com>
2024-04-02 19:07:07 +0200

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Square class definition"""
+
+
+class Square:
+    """Class representing a square"""
+
+    def __init__(self, size):
+        """
+        A class that defines a sqaure with a private instance.
+
+        Attribtes:
+            __size (int): size of a side of the square
+
+        Methods:
+            None
+        """
+        self.__size = size
