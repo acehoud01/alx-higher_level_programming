df12bfb89578c549ef9835bcd2d51bc4ccfe4560
Anele Bilikwa <aceamahle1@gmail.com>
2024-04-15 23:46:29 +0200

    10-square.py

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+'''
+10-square.py
+'''
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    inherits from Rectangle class
+    """
+
+    def __init__(self, size):
+        """
+        Defining a rectangle
+        """
+        if self.integer_validator('size', size):
+            self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """
+        Returns area of Square object
+        """
+        return super().area()
