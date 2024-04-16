2fd1c47888e54cc8623f0e33ce3fd5a8a9144923
Anele Bilikwa <aceamahle1@gmail.com>
2024-04-16 22:39:12 +0200

    12-pascal_triangle.py

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,27 +1,19 @@
 #!/usr/bin/python3
+"""Pascal's Triangle function"""
 
 
 def pascal_triangle(n):
-    """
-    Generates Pascal's triangle up to the nth row.
-
-    Args:
-    n (int): The number of rows in the Pascal's triangle.
-
-    Returns:
-    list: A list of lists representing Pascal's triangle.
+    """Represents Pascal's Triangle of size n
     """
     if n <= 0:
         return []
 
     triangle = [[1]]
-
-    for i in range(1, n):
-        new_row = [1]
-        for j in range(1, i):
-            new_value = triangle[i-1][j-1] + triangle[i-1][j]
-            new_row.append(new_value)
-        new_row.append(1)
-        triangle.append(new_row)
-
+    while len(triangle) != n:
+        t = triangle[-1]
+        tmp = [1]
+        for i in range(len(t) - 1):
+            tmp.append(t[i] + t[i + 1])
+        tmp.append(1)
+        triangle.append(tmp)
     return triangle
