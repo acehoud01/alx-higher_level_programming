0378e345a495306c977d8f7b29c7d557ad3c0fde
Anele Bilikwa <aceamahle1@gmail.com>
2024-04-22 20:45:43 +0200

    rectangle init commit

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,110 @@
+#!/usr/bin/python3
+'''
+Module for Rectangle class
+'''
+
+from models.base import Base
+
+
+class Rectangle(Base):
+    """Rectangle class that inherits from Base."""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        Initializes the Rectangle class instance.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+            x (int, optional): The x-coordinate of the rectangle.
+            y (int, optional): The y-coordinate of the rectangle.
+            id (int, optional): An optional integer ID for the object.
+            If not provided, a new unique ID is assigned. Defaults to None.
+        """
+        super().__init__(id)  # Call Base class constructor with id
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """
+        Getter for the width attribute.
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+        Setter for the width attribute.
+
+        Args:
+            value (int): The new value for the width. Raises an error
+                if not an integer greater than zero.
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be greater than 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """
+        Getter for the height attribute.
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+        Setter for the height attribute.
+
+        Args:
+            value (int): The new value for the height. Raises an error
+                if not an integer greater than zero.
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be greater than 0")
+        self.__height = value
+
+    @property
+    def x(self):
+        """
+        Getter for the x attribute.
+        """
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """
+        Setter for the x attribute.
+
+        Args:
+            value (int): The new value for the x-coordinate.
+        """
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        self.__x = value
+
+    @property
+    def y(self):
+        """
+        Getter for the y attribute.
+        """
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """
+        Setter for the y attribute.
+
+        Args:
+            value (int): The new value for the y-coordinate.
+        """
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        self.__y = value
