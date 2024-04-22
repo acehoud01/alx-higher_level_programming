8eafbe3ab7e775cad99e1fa47995693078d7e1a7
Anele Bilikwa <aceamahle1@gmail.com>
2024-04-22 19:34:09 +0200

    base.py init commit

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+class Base:
+    """Base class for managing id attribute and object count."""
+
+    __nb_objects = 0  # Private class attribute for object count
+
+    def __init__(self, id=None):
+        """
+        Initializes the Base class instance.
+
+        Args:
+            id (int, optional): An optional integer ID for the object.
+                If not provided, a new unique ID is assigned. Defaults to None.
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
