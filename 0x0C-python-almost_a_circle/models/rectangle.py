4570d4683b21846f4d6c3a9b205c79faa72d7b2d
Anele Bilikwa <aceamahle1@gmail.com>
2024-04-23 18:04:43 +0200

    rectangle class

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -81,12 +81,14 @@ class Rectangle(Base):
 
     def display(self):
         """Display the rectangle with '#' characters."""
+        for _ in range(self.y):
+            print()
         for _ in range(self.height):
-            print("#" * self.width)
+            print(" " * self.x + "#" * self.width)
 
     def __str__(self):
         """Return string representation of Rectangle."""
         return (
-                f"[Rectangle] ({self.id}) {self.x}/{self.y} - "
-                f"{self.width}/{self.height}"
-                )
+            f"[Rectangle] ({self.id}) {self.x}/{self.y} - "
+            f"{self.width}/{self.height}"
+        )
