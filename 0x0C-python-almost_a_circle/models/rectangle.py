14e9d4b262ef97c6f78d510fa3964812feb63cc2
Anele Bilikwa <aceamahle1@gmail.com>
2024-04-23 17:54:16 +0200

    rectangle class

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -78,3 +78,8 @@ class Rectangle(Base):
     def area(self):
         """Calculate and return the area of the rectangle."""
         return self.width * self.height
+
+    def display(self):
+        """Display the rectangle with '#' characters."""
+        for _ in range(self.height):
+            print("#" * self.width)
